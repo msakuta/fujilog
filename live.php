7b44bbf822b2de720c653872a4db9199bc7ef3a0
msakuta <masahiro.sakuta@gmail.com>
2013-11-30 18:26:49 +0900

    Removed unused codes to prevent runtime errors in live.php

--- live.php
+++ live.php
@@ -2,17 +2,6 @@
 <?php htmlHeader(); ?>
 <h1>Mt. Fuji Live Camera</h1>
 <?php
-$now = new DateTime();
-$now->sub(new DateInterval('P1M'));
-$dir = $now->format('Y-m-d');
-$file = $now->format('Y-m-d-H-i') . '.jpg';
-//echo 'dir: ', $dir, "<br>\n";
-//echo 'file: ', $file, "<br>\n";
-echo 'time: ', $now->format('Y-m-d-H-i-s'), "<br>\n";
-//echo '<image src="../zenphoto/albums/fujilog/',
-//  $dir, '/', $file, '"><br>' . "\n";
-//echo '<image src="', $dir, '/', $file, '">' . "<br>\n";
-echo "<br>\n";
 echo 'Last successful upload: '; 
 
 $fp = fopen('current', 'r');
